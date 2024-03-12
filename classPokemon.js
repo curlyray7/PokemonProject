3a672d45697149ae3e3b0c8f015bacffac00edeb
curlyray7 <melyouns2999@gmail.com>
2024-03-12 16:18:18 +0100

    constructeur class Pokemon

--- classPokemon.js
+++ classPokemon.js
@@ -1,1 +1,17 @@
-let pokemon
+// Returns a JSON array where each element is a dict containing :
+// - base_attack,
+// - base_defense,
+// - base_stamina, 
+// - pokemon_ID,  
+// - pokemon name.
+
+class Pokemon { // nouvelle déclaration
+    constructor (name, id, attack, defense, stamina) {
+        this.pokemon_name = name;
+        this.pokemon_id = id;
+        this.base_attack;
+        this.base_defense;
+        this.base_stamina;
+    }
+    toString() { return `${this.nom} a un rayon de ${this.rayon}km` }
+}
