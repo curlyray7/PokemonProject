ba23c890b28e1813fc21cae14180298b9810f5ae
curlyray7 <melyouns2999@gmail.com>
2024-03-12 16:24:40 +0100

    dsds

--- classPokemon.js
+++ classPokemon.js
@@ -9,9 +9,9 @@ class Pokemon { // nouvelle déclaration
     constructor (name, id, attack, defense, stamina) {
         this.pokemon_name = name;
         this.pokemon_id = id;
-        this.base_attack;
-        this.base_defense;
-        this.base_stamina;
+        this.base_attack = attack;
+        this.base_defense = defense;
+        this.base_stamina = stamina;
     }
     toString() { return `${this.nom} a un rayon de ${this.rayon}km` }
 }
