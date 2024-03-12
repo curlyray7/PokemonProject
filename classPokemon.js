4c0357726b8e5fe58996800a268b4c78e8ad1ee6
Raphaelo04 <raphael.lebouedec22@gmail.com>
2024-03-12 17:12:50 +0100

    modif 12/03

--- classPokemon.js
+++ classPokemon.js
@@ -5,7 +5,7 @@
 // - pokemon_ID,  
 // - pokemon name.
 
-class Pokemon { // nouvelle déclaration
+class Pokemon { 
     constructor (name, id, attack, defense, stamina) {
         this.pokemon_name = name;
         this.pokemon_id = id;
@@ -13,11 +13,51 @@ class Pokemon { // nouvelle déclaration
         this.base_defense = defense;
         this.base_stamina = stamina;
     }
+
+    // Méthode pour récupérer la liste des types (objets Type)
+    getTypes() {
+        return this.types;
+    }
+
+
     toString() { 
         return `${this.pokemon_name} 
         \nID : ${this.pokemon_id}
         \nAttack : ${this.base_attack}
         \nDefense : ${this.base_defense}
         \nStamina : ${this.base_stamina}` 
+    }
+}
+
+
+
+class Type {
+    constructor() {
+        this.all_types = {};
+        this.loadTypes(); 
+    }
+
+    // Méthode pour charger les types de Pokémon depuis le fichier pokemon_type.js
+    loadTypes() {
+        const pokemon_types = require('./JS/pokemon_type.js');
+        for (const pokemon of pokemon_types) {
+            if (pokemon.form === "Normal") {
+                this.all_types[pokemon.pokemon_name] = pokemon.type;
+            }
+        }
+    }
+
+    // Méthode pour calculer l'efficacité d'un type d'attaque contre un type de défenseur
+    effectiveness(attack_type, defender_type) {
+        const type_effectiveness = require('./JS/type_effectiveness.js');
+        if (!type_effectiveness.hasOwnProperty(attack_type) || !type_effectiveness[attack_type].hasOwnProperty(defender_type)) {
+            return "Type not found or effectiveness not defined.";
+        }
+        return type_effectiveness[attack_type][defender_type];
+    }
+
+    // Méthode pour afficher les types de Pokémon
+    toString() {
+        return Object.keys(this.all_types).join(', ');
     }
 }
